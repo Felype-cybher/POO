61216f50be84fcfd6bd02b380e2e63b599cf821e
Felype <felypemaxsantossilva0@gmail.com>
2025-04-03 14:36:35 -0300

    Update TesteCarro.java

--- TesteCarro.java
+++ TesteCarro.java
@@ -3,12 +3,11 @@ package br.com.Felype.atividade02;
 public class TesteCarro {
     public static void main(String[] args) {
         Carro carro1 = new Carro();
-
-        carro1.cor = "Preto";
-        carro1.ano = 2000;
-        carro1.marca = "Toyota";
-        carro1.velocidadeAtual = 25;
-        carro1.modelo="Corolla";
+        carro1.setMarca("Toyota");
+        carro1.setAno(2003);
+        carro1.setCor("Preto");
+        carro1.setModelo("Corolla");
+        carro1.setVelocidadeAtual(25);
 
         carro1.imprimirDetalhes();
         carro1.acelerar();
